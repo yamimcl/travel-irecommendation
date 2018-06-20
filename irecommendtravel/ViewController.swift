ae704860098c39ac949ddd194c4a231ec7659f40
Girls Who Code <girlswhocode@mb2016-dclarke-3.local>
2018-06-20 13:53:21 -0400

    Initial Commit

---
+++ irecommendtravel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  irecommendtravel
+//
+//  Created by Girls Who Code on 6/20/18.
+//  Copyright © 2018 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
